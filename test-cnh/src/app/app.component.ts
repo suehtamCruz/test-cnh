1b1f9c660c3e949748376729e24fd7313c57ac69
matheus <matheuscz3110@gmail.com>
2025-01-21 15:01:42 -0300

    initial commit

--- test-cnh/src/app/app.component.ts
+++ test-cnh/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import ScanbotSDK from 'scanbot-web-sdk';
+import type { IDocumentScannerHandle } from 'scanbot-web-sdk/@types';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'test-cnh';
+  private documentScanner!: IDocumentScannerHandle;
+  constructor() {
+    ScanbotSDK.initialize({
+      licenseKey: '',
+    });
+  }
+
+  ngAfterViewInit(): void {
+    console.log('scanner', document.getElementById('scanner'));
+    ScanbotSDK.instance.createDocumentScanner({
+      container: document.getElementById('scanner') as HTMLElement,
+    });
+  }
 }
