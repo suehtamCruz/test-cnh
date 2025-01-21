339548523c1831c475fcf0ffb5ff66c137dcf0f9
matheus <matheuscz3110@gmail.com>
2025-01-21 15:31:10 -0300

    added camera options

--- test-cnh/src/app/app.component.ts
+++ test-cnh/src/app/app.component.ts
@@ -12,14 +12,50 @@ export class AppComponent implements AfterViewInit {
   private documentScanner!: IDocumentScannerHandle;
   constructor() {
     ScanbotSDK.initialize({
-      licenseKey: '',
+      licenseKey:
+        'dg5WgphDzZcb2YQNiPZ8Q3qRMz0Ktw' +
+        'UQ6Ha+VUWtcsvdchmp8PeJgtGabUB2' +
+        'siI+zd+3scGH0WGDv4DmUNINC9JKym' +
+        '2vuPugLKawvJ3OxHsWtGWFv3mTM61a' +
+        'IKYMgxSibTY3B0wTMOso5WnFZGTg7C' +
+        'aDt21wRgMQm6c1anIiqHba14GVVESM' +
+        '4U98RIORKTLp5BfgKmJjMlDYtK6Uhw' +
+        '7UtlYJi4sYLaNcV1plM2Fx3cJEy4zV' +
+        'zI4vrU+u56ylvuykBc38QZ56cepatC' +
+        'eGkXS4LteKAwAqkE9IUHg32ItNWFjw' +
+        'Yx3g6qJTQcFqEkMpV1auFYNC0RU2Zf' +
+        'd7xN2zLHI7qQ==\nU2NhbmJvdFNESw' +
+        'psb2NhbGhvc3R8dGVzdC1jbmgtOXk1' +
+        'My52ZXJjZWwuYXBwCjE3MzgxMDg3OT' +
+        'kKODM4ODYwNwo4\n',
     });
   }
 
   ngAfterViewInit(): void {
-    console.log('scanner', document.getElementById('scanner'));
     ScanbotSDK.instance.createDocumentScanner({
-      container: document.getElementById('scanner') as HTMLElement,
+      containerId: 'scanner',
+      autoCaptureEnabled: false,
+      onDocumentDetected: (res) => {
+        console.log('onDocumentDetected', res);
+      },
+      onCaptureButtonClick: (res) => {
+        console.log('onCaptureButtonClick', res);
+      },
+      onError: (res) => {
+        console.log('onError', res);
+      },
+      spinnerColor: '#00b131',
+      videoConstraints: {
+        facingMode: 'environment',
+        resizeMode: 'none',
+        width: { ideal: 800, max: 900 },
+        height: { ideal: 600,max: 900 },
+        experimental: {
+          focusMode: 'continous',
+          focusDistance: 0,
+        },
+      } as MediaTrackConstraints,
     });
+    console.log('scanner', document.getElementById('scanner'));
   }
 }
