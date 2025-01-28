f0e7fde17474bfbe87c1944f5d9cee384819de59
matheus <matheuscz3110@gmail.com>
2025-01-28 19:28:35 -0300

    bhnmjbj,,bm

--- test-cnh/src/app/app.module.ts
+++ test-cnh/src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ZXingScannerModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
